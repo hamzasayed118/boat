39530f4e072939b382dbf2981b6c85bd5753275b
hamzasayed118 <102883259+hamzasayed118@users.noreply.github.com>
2022-08-05 15:39:27 -0700

    Add files via upload

---
+++ js/boat.js
@@ -0,0 +1,27 @@
+class Boat{
+    constructor(x,y,width,height,boatPos) {
+      var options={
+        restitution:0.6,
+        friction:1,
+        density:1,
+      };
+      this.body = Bodies.rectangle(x,y,width.height,options);
+      this.width=width
+      this.height=height
+      this.image= loadImage("./assets/boat.png");
+      this.boatPosition=boatPos
+      World.add(world,this.body);
+    }
+
+    display() {
+    var pos= this.body.position;
+    push()
+    translate(pos.x,pos.y)
+    imageMode(CENTER)
+    image(this.image,0,this.boatPosition,this.width,this.height);
+    pop()
+
+
+    }
+
+}
